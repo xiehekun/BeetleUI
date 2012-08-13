b0b0017aa08feae8b7e074cbc11bfabfba167e4e
xiehekun <xiehekun@gmail.com>
2012-08-13 23:12:03 +0800

    update layout

--- js/layout.js
+++ js/layout.js
@@ -8,7 +8,7 @@ angular.module('layout',[],function($routeProvider,$locationProvider){
 	$routeProvider.when('/resources',{templateUrl:'app/resources/resources.html',controller:ResCtrl});
 	$routeProvider.when('/fault',{templateUrl:'app/fault/fault.html',controller:FaultCtrl});
 	$routeProvider.when('/system',{templateUrl:'app/system/system.html',controller:SysCtrl});
-	$routeProvider.when('/home',{templateUrl:'app/home/homepage.html',controller:SysCtrl});
+	$routeProvider.when('/home',{templateUrl:'app/home/homepage.html',controller:HomeCtrl});
 	$routeProvider.otherwise({redirectTo:'/home',templteUrl:'app/home/homepage.html',controller:HomeCtrl});
 });
 
@@ -16,24 +16,5 @@ function MainCtrl($scope,$location){
 	$scope.routeTo=function(uri){
 		$location.path(uri);
 	}
-}
-
-function AppCtrl($scope,$location){
-	
-}
-function CatalogCtrl($scope,$location){
-	
-}
-function ResCtrl($scope,$location){
-	
-}
-function FaultCtrl($scope,$location){
-	
-}
-function SysCtrl($scope,$location){
-	
-}
-function HomeCtrl($scope,$location){
-	
 }
 
